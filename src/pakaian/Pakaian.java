3bbb14abd11cac9e31140e6059ecaec11ff5b4ad
ftama0 <68576999+ftama0@users.noreply.github.com>
2020-11-16 01:52:01 +0800

    Constructor singleton_baju
    Membuat Constructor singleton_baju

--- src/pakaian/Pakaian.java
+++ src/pakaian/Pakaian.java
@@ -14,8 +14,4 @@ public class Pakaian {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
 }
