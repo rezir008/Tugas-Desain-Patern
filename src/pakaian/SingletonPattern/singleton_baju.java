d9e08f34cb9a9ea74b319b8d9c0065e89617ef28
ftama0 <68576999+ftama0@users.noreply.github.com>
2020-11-16 22:15:28 +0800

    Tambah instance singleton_baju
    Membuat dan menambahkan instance singleton_baju dalam class singleton_baju

--- src/pakaian/SingletonPattern/singleton_baju.java
+++ src/pakaian/SingletonPattern/singleton_baju.java
@@ -10,5 +10,13 @@ package pakaian.SingletonPattern;
  * @author FERRY PRATAMA
  */
 public class singleton_baju {
-    
+    private static singleton_baju instance; //membuat instance/objek singleton_baju
+  
+  private singleton_baju(){} //mode private agar tidak dapat di instansiasi(pembuatan instance/objek) oleh class lain
+  public static singleton_baju GetInstance() // Menggantikan objek singleto_baju menggunakan GetInstance, untuk memanggil di class lain
+  {
+    if(instance == null){ //cek instance/objek bernilai null atau tidak
+     instance = new singleton_baju();}//jika null maka buat instance/objek baru
+    return instance; }
+//Pengecekan di atas memastikan bahwa hanya akan ada satu instance/objek dari kelas tersebut
 }
