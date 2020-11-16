3268da7f10cd702047b480d5df0293f93535fa83
ftama0 <68576999+ftama0@users.noreply.github.com>
2020-11-16 22:29:18 +0800

    Test 2 : Tambah kondisi baru untuk test class singleton  memiliki satu instance dengan membandingkan objek
     tambah kondisi baru untuk test ke-2 dengan membandingkan 2 objek yang menggunakan GetInstance dan operator ==
    
    cek singleton benar-benar hanya selalu memiliki satu instance
     True singleton : hasil sama
     false singelton : hasil beda
    
    cek singleton benar-benar hanya selalu memiliki satu instance

--- src/pakaian/Pakaian.java
+++ src/pakaian/Pakaian.java
@@ -18,6 +18,25 @@ public class Pakaian {
         singleton_baju baju = singleton_baju.GetInstance(); //membuat objek baru dengan menggunakan method GetInstance
         baju.bahan_baju(); //Gunakan objek baju untuk panggil method bahan_baju di class singleton_baju untuk test class singleton berfungsi
         
+         //Test ke-2 membandingkan 2 objek dengan menggunakan GetInstance dan operator ==
+        //True singleton/satu instance : hasil sama
+        //false singelton : hasil beda
+         System.out.println("\n ====== Test ke-2  ===== \n ");
+         singleton_baju baju_kemeja = singleton_baju.GetInstance();
+         singleton_baju baju_kaos = singleton_baju.GetInstance();
+         if(baju_kemeja!=baju_kaos) //cek singleton benar-benar hanya selalu memiliki satu instance
+         {
+             System.out.println("Baju kemeja dan baju kaos");
+             System.out.println("Bahan baju Tidak sama : Singleton memiliki lebih dari satu instance/objek"); 
+             System.out.println("\n Falsesingleton : hasil tidak sama");
+         }
+         else if(baju_kemeja==baju_kaos)
+         {
+             System.out.println("Baju kemeja dan baju kaos");
+             System.out.println("Bahan baju sama : Singleton benar-benar hanya selalu memiliki satu instance/objek");
+             System.out.println("\n True singleton");
+         }
+        System.out.println("* =============================== *");
         //demo Adapter
         System.out.println("");
         System.out.println("**Adapter**");
@@ -26,7 +45,7 @@ public class Pakaian {
         jbtersedia.cekjb("kaos");
         jbtersedia.cekjb("gaun");
         jbtersedia.cekjb("jaket");
-        
+        System.out.println("* =============================== *");
         //Demonstrasi Template Method//
         TemplateMethod presentasi = new TemplateMethod_TrenchCoat();
         presentasi.mulai();
