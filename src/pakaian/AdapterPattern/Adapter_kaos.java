e00f5da7117b6667c9f0f22af4a63ee33219c9cf
rezir008 <rezir008@gmail.com>
2020-11-16 18:37:25 +0800

    membuat 2 class adalah 2 jenis baju baru

---
+++ src/pakaian/AdapterPattern/Adapter_kaos.java
@@ -0,0 +1,21 @@
+package pakaian.AdapterPattern;
+
+public class Adapter_kaos implements Adapter_baju_new{
+
+    @Override
+    public void kaos(String jbaju) {
+        System.out.println("bahan "+ jbaju + " yang tersedia adalah :");
+        System.out.println("1. Cotton Combed");
+        System.out.println("2. Cotton Carded");
+        System.out.println("3. Viscose");
+        System.out.println("4. Cotton Viscose");
+        System.out.println("5. Polyester");
+        System.out.println("6. Hyget");
+    }
+
+    @Override
+    public void jaket(String jbaju) {
+        
+    }
+    
+}
