008e22cb8caa757515392a02bf226c4753ab83fa
ftama0 <68576999+ftama0@users.noreply.github.com>
2020-11-16 19:44:27 +0800

    Tambah objek baju di main pakaian
    tambah objek baju di main pakaian  menggunakan method GetInstance

--- src/pakaian/Pakaian.java
+++ src/pakaian/Pakaian.java
@@ -3,6 +3,12 @@ import pakaian.SingletonPattern.singleton_baju;
 
 public class Pakaian {
     public static void main(String[] args) {
+        //demo Singleton
+        
+        //Test ke-1 dengan panggil method turunan
+        System.out.println("**Singleton**");
+        System.out.println("\n ====== Test ke-1  ===== \n ");
+        singleton_baju baju = singleton_baju.GetInstance(); //membuat objek baru dengan menggunakan method GetInstance
        
     }
     
