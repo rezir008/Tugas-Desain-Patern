d1e7358cba32c3332e3411243015bbfa9079756f
rezir008 <rezir008@gmail.com>
2020-11-16 18:41:11 +0800

    memberi jarak dengan desain pattern lainnya

--- src/pakaian/Pakaian.java
+++ src/pakaian/Pakaian.java
@@ -5,6 +5,9 @@ import pakaian.AdapterPattern.Adapter_jbtersedia;
 public class Pakaian {
     public static void main(String[] args) {
         
+        //demo Adapter
+        System.out.println("");
+        System.out.println("**Adapter**");
         Adapter_jbtersedia jbtersedia = new Adapter_jbtersedia();
         jbtersedia.cekjb("kemeja");
         jbtersedia.cekjb("kaos");
