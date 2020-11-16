f6ef9746e50afb7b7358a3932badcea2c19c8e6c
ftama0 <68576999+ftama0@users.noreply.github.com>
2020-11-16 22:21:46 +0800

    Test-1 : Tambah objek baju di main pakaian dan Gunakan objek baju utnuk panggil method bahan_baju
    Tambah objek baju di main pakaian  menggunakan method GetInstance dan Gunakan objek baju utnuk panggil method bahan_baju di class singleton_baju untuk test class singleton berfungsi

--- src/pakaian/Pakaian.java
+++ src/pakaian/Pakaian.java
@@ -10,7 +10,13 @@ import pakaian.SingletonPattern.singleton_baju;
 public class Pakaian {
     public static void main(String[] args) {
         
+         //demo Singleton
         
+        //Test ke-1 dengan panggil method turunan
+        System.out.println("**Singleton**");
+        System.out.println("\n ====== Test ke-1  ===== \n ");
+        singleton_baju baju = singleton_baju.GetInstance(); //membuat objek baru dengan menggunakan method GetInstance
+        baju.bahan_baju(); //Gunakan objek baju untuk panggil method bahan_baju di class singleton_baju untuk test class singleton berfungsi
         
         //demo Adapter
         System.out.println("");
